1a156920ec0e8e8273b3854c03859ce39a096871
ishigen425 <g4.fine.yamanashi@gmail.com>
2020-12-03 20:51:49 +0900

    modify make_gallery

--- tool/make_gallery.py
+++ tool/make_gallery.py
@@ -1,9 +1,13 @@
+from PIL import Image
 import os
 
 IMG_DIR = "../src/img/"
-IMG_DIR_HTML = "img/"
-HTML_DIR = "../src"
-IMG_TEMPLATE_HTML = "        <img src=\"{}\">"
+IMG_DIR_HTML = "../img/"
+HTML_DIR = "../src/gallery/"
+IMG_TEMPLATE_HTML = "          <div class=\"column\"><img src=\"{}\"></div>"
+OPEN_COLUMN_HTML = "<div class=\"columns is-desktop\">"
+END_COLUMN_HTML = "</div>"
+COLUMN_TAMPLATE_HTML = "<div class=\"column\"></div>"
 
 dir_list = os.listdir(IMG_DIR)
 
@@ -14,9 +18,17 @@ with open('template.html') as f:
 for d in dir_list:
     img_contents = []
     title = d
-    for img_path in os.listdir(os.path.join(IMG_DIR, d)):
+    img_contents.append(OPEN_COLUMN_HTML)
+    for idx, img_path in enumerate(os.listdir(os.path.join(IMG_DIR, d))):
+        if idx % 3 == 0 and idx > 0:
+            img_contents.append(END_COLUMN_HTML)
+            img_contents.append(OPEN_COLUMN_HTML)
         img_contents.append(IMG_TEMPLATE_HTML.format(os.path.join(IMG_DIR_HTML, d, img_path)))
-    html = html_template.replace("%title%", title).replace("%gallery%", "\n".join(img_contents))
+    while idx % 3 != 2:
+        img_contents.append(COLUMN_TAMPLATE_HTML)
+        idx += 1
+    img_contents.append(END_COLUMN_HTML)
+    html = html_template.replace("%gallery%", "\n".join(img_contents))
     with open(os.path.join(HTML_DIR, d) + ".html", mode="w") as f:
         f.write(html)
     
---
+++ tool/test.py
@@ -0,0 +1,9 @@
+import os
+
+from PIL import Image
+
+img_path = "../src/img/190505_13h_tochigi/D5zDsHAU0AEis6a.jpg"
+img = Image.open(img_path)
+img_resize = img.resize((img.width // 2, img.height // 2))
+img_resize.save(img_path + ".jpg")
+
