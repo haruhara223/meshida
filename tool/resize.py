8b59b169b14e478eddb96a92d358a840eae94fc0
ishigen425 <g4.fine.yamanashi@gmail.com>
2021-01-05 21:55:32 +0900

    二回以上実行されないように修正

--- tool/resize.py
+++ tool/resize.py
@@ -14,7 +14,7 @@ for d in dir_list:
     for img_file_name in os.listdir(os.path.join(IMG_DIR, d)):
         if img_file_name in (TOP_IMG_BIG_FILE_NAME, TOP_IMG_FILE_NAME): continue
         img = Image.open(os.path.join(IMG_DIR, os.path.join(d, img_file_name)))
-        if max(img.width, img.height) >= 800:
+        if max(img.width, img.height) >= 850:
             scale_rate = max(img.width, img.height) / 800
             img_resize = img.resize((int(img.width // scale_rate), int(img.height // scale_rate)))
             img_resize.save(os.path.join(IMG_DIR, os.path.join(d, img_file_name)))
